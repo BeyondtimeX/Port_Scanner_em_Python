30bcbc057512ffd411b8f0ca553eeed01e70fe89
Bruno Almeida <bruno.desouzaads@GMAIL.com>
2024-05-18 10:40:04 -0300

    Update projeto_nmap.py

--- projeto_nmap.py
+++ projeto_nmap.py
@@ -1,17 +1,51 @@
 import nmap
+import re
 
-# Inicializando o scanner
-scanner = nmap.PortScanner()
+def imprimir_cabecalho():
+    """Imprime o cabeçalho do programa."""
+    print('\n' + '-=' * 18)
+    print('\tScanner de Portas')
+    print('-=' * 18)
 
-# Cabeçalho do programa
-print('\n' + '-=' * 18)
-print('\tScanner de Portas')
-print('-=' * 18)
+def solicitar_ip():
+    """Solicita e retorna um IP ou hostname válido do usuário."""
+    while True:
+        ip = input('\nEntre com o IP ou Hostname a ser varrido: ')
+        if validar_ip(ip):
+            return ip
+        else:
+            print('IP ou Hostname inválido. Por favor, tente novamente.')
 
-# Solicitação inicial do IP
-ip = input('\nEntre com o IP ou Hostname a ser varrido: ')
+def validar_ip(ip):
+    """Valida se o IP ou hostname fornecido é válido."""
+    ip_pattern = re.compile(
+        r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"  # Formato de IPv4
+        r"|^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*"  # Formato de hostname
+        r"([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$"
+    )
+    return re.match(ip_pattern, ip) is not null
+
+def exibir_menu():
+    """Exibe o menu de opções e retorna a escolha do usuário."""
+    while True:
+        try:
+            escolha = int(input('\nEscolha o tipo de varredura a ser realizado:\n'
+                                '\t<1> Varredura do tipo SYN\n'
+                                '\t<2> Varredura do tipo UDP\n'
+                                '\t<3> Varredura do tipo intensa\n'
+                                '\t<4> Mudar o IP\n'
+                                '\t<5> Salvar resultados em log\n'
+                                '\t<0> Sair\n'
+                                '\tDigite a opção escolhida: '))
+            if escolha in range(0, 6):
+                return escolha
+            else:
+                print('Opção inválida, por favor digite um número entre 0 e 5.')
+        except ValueError:
+            print('Opção inválida, por favor digite um número entre 0 e 5.')
 
 def realizar_varredura(ip, tipo_varredura, protocolo):
+    """Realiza a varredura no IP fornecido usando o tipo de varredura especificado e imprime os resultados."""
     print('\nVersão do NMAP: ', scanner.nmap_version())
     scanner.scan(ip, '1-1024', tipo_varredura)
     print(scanner.scaninfo())
@@ -19,39 +53,48 @@ def realizar_varredura(ip, tipo_varredura, protocolo):
     protocolos = scanner[ip].all_protocols()
     print('Protocolos encontrados: ', protocolos)
     if protocolo in protocolos:
-        print('\nPortas Abertas: ', scanner[ip][protocolo].keys(), '\n')
+        portas_abertas = list(scanner[ip][protocolo].keys())
+        print('\nPortas Abertas: ', portas_abertas, '\n')
+        return portas_abertas
     else:
         print(f'Nenhuma porta {protocolo} aberta encontrada.\n')
+        return []
 
-while True:
-    # Exibindo menu de opções
-    try:
-        menu = int(input('\nEscolha o tipo de varredura a ser realizado:\n\t<1> Varredura do tipo SYN\n\t<2> Varredura do tipo UDP'
-                         '\n\t<3> Varredura do tipo intenso\n\t<4> Mudar o IP\n\t<0> Sair\n\tDigite a opção escolhida: '))
-    except ValueError:
-        print('Opção inválida, por favor digite um número entre 0 e 4.')
-        continue
+def salvar_resultados(ip, resultados):
+    """Salva os resultados da varredura em um arquivo de log."""
+    with open("resultados_varredura.txt", "a") as file:
+        file.write(f"\nResultados da varredura para o IP: {ip}\n")
+        for tipo, portas in resultados.items():
+            file.write(f"\nTipo de Varredura: {tipo}\n")
+            file.write(f"Portas Abertas: {portas}\n")
+        file.write("-" * 40 + "\n")
+    print('Resultados salvos no arquivo resultados_varredura.txt')
 
-    if menu == 1:
-        # Varredura do tipo SYN
-        realizar_varredura(ip, '-sS', 'tcp')
+def main():
+    """Função principal que executa o fluxo do programa."""
+    global scanner
+    scanner = nmap.PortScanner()
+    imprimir_cabecalho()
+    ip = solicitar_ip()
+    resultados = {}
 
-    elif menu == 2:
-        # Varredura do tipo UDP
-        realizar_varredura(ip, '-sU', 'udp')
+    while True:
+        menu = exibir_menu()
 
-    elif menu == 3:
-        # Varredura intensa
-        realizar_varredura(ip, '-sC', 'tcp')
+        if menu == 1:
+            resultados['SYN'] = realizar_varredura(ip, '-sS', 'tcp')
+        elif menu == 2:
+            resultados['UDP'] = realizar_varredura(ip, '-sU', 'udp')
+        elif menu == 3:
+            resultados['Intensa'] = realizar_varredura(ip, '-sC', 'tcp')
+        elif menu == 4:
+            ip = solicitar_ip()
+            resultados = {}  # Resetar resultados ao mudar de IP
+        elif menu == 5:
+            salvar_resultados(ip, resultados)
+        elif menu == 0:
+            print('\nFim do Programa.\n')
+            break
 
-    elif menu == 4:
-        # Mudança de IP
-        ip = input('\nEntre com o novo IP ou Hostname a ser varrido: ')
-
-    elif menu == 0:
-        # Finalização do programa
-        print('\nFim do Programa.\n')
-        break
-
-    else:
-        print('Opção inválida, escolha uma das opções solicitadas.')
+if __name__ == "__main__":
+    main()
